edb6bab71d51620ab0dec32fb640ec55afaad9ac
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-04-19 03:26:45 +0530

    added danktheme to ghostty

--- modules/users/daksh.nix
+++ modules/users/daksh.nix
@@ -206,7 +206,7 @@ in
             font-family = "Maple Mono NF";
             font-size = 14;
             font-feature = "-calt,-zero,-cv02,+cv01,+cv61";
-            # config-file = "./themes/dankcolors";
+            config-file = "./themes/dankcolors";
             app-notifications = "no-clipboard-copy,no-config-reload";
           };
         };
