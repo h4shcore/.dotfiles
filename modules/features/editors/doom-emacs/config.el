dda1818d5295c65c529dfe5b5d3f7eaf61eb8292
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-04-25 01:07:33 +0530

    added helix mode in doom-emacs

--- modules/features/editors/doom-emacs/config.el
+++ modules/features/editors/doom-emacs/config.el
@@ -1,76 +1,99 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-(setq doom-font (font-spec :family "Maple Mono NF" :size 15 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "Maple Mono NF" :size 15))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
+;; UI
 (add-to-list 'custom-theme-load-path "~/.cache/emacs/themes")
 (setq doom-theme 'dankcolors)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
+(setq doom-font (font-spec :family "Maple Mono NF" :size 14.0)
+      doom-variable-pitch-font (font-spec :family "Maple Mono NF" :size 14.0))
+(add-to-list 'default-frame-alist '(font . "Maple Mono NF-15:cv01=1:cv61=1:calt=0:zero=0:cv02=0"))
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+;; Editing
+(setq-default visual-fill-column-width 90
+              word-wrap t)
+(add-hook 'text-mode-hook #'visual-line-mode)
+(add-hook 'prog-mode-hook #'visual-line-mode)
+(add-hook 'prog-mode-hook #'rainbow-mode)
+(setq scroll-conservatively 101
+      scroll-margin 1)
 
+;; HEL (Helix Emacs Lateral) - Helix-like keybindings
+(use-package! hel
+  :config
+  (hel-mode 1))
 
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `with-eval-after-load' block, otherwise Doom's defaults may override your
-;; settings. E.g.
-;;
-;;   (with-eval-after-load 'PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look them up).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+;; Completion
+(after! company
+  (setq company-idle-delay 0.2))
+(map! :i "C-SPC" #'company-complete)
+
+;; Indent bars
+(use-package! indent-bars
+  :hook (prog-mode . indent-bars-mode)
+  :config
+  (setq indent-bars-pattern "."
+        indent-bars-width-frac 0.15
+        indent-bars-pad-frac 0.1
+        indent-bars-zigzag nil
+        indent-bars-display-on-blank-lines t))
+
+;; Tabs
+(after! centaur-tabs
+  (setq centaur-tabs-style "bar"
+        centaur-tabs-height 28
+        centaur-tabs-set-bar 'under
+        centaur-tabs-show-count nil))
+
+;; Direnv
+(after! envrc
+  (envrc-global-mode))
+
+;; LSP
+(after! eglot
+  (setq eglot-ignored-server-capabilities '())
+  (add-hook 'eglot-managed-mode-hook
+            (lambda ()
+              (eglot-inlay-hints-mode 1))))
+
+;; Modeline
+(after! doom-modeline
+  (setq doom-modeline-buffer-file-name-style 'truncate-from-project
+        doom-modeline-vcs-max-length 24
+        doom-modeline-buffer-encoding t
+        doom-modeline-check-simple-format nil))
+
+;; Flymake
+(after! flymake
+  (setq flymake-show-diagnostics-at-end-of-line 'short))
+
+;; Vertico
+(after! vertico
+  (setq completion-ignored-extensions nil))
+
+;; Projectile
+(after! projectile
+  (setq projectile-globally-ignored-directories nil))
+
+;; Keybindings
+(map! "M-j"
+      (cmd!
+       (let ((col (current-column)))
+         (forward-line 1)
+         (transpose-lines 1)
+         (forward-line -1)
+         (move-to-column col)))
+      "M-k"
+      (cmd!
+       (let ((col (current-column)))
+         (transpose-lines 1)
+         (forward-line -2)
+         (move-to-column col)))
+      "C-a"
+      (cmd!
+       (dolist (buf (buffer-list))
+         (with-current-buffer buf
+           (when (and buffer-file-name (not (buffer-modified-p)))
+             (revert-buffer t t t)))))
+      "C-i" #'eglot-inlay-hints-mode
+      "C-r" #'undo-fu-only-redo
+      "C-S-r" (cmd! (revert-buffer t t t)))
