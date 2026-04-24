56722c1f16eb5caadf0925fa4a56f373f99b6a52
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-04-25 00:35:40 +0530

    added doom emacs configs

--- modules/features/editors/doom-emacs/config.el
+++ modules/features/editors/doom-emacs/config.el
@@ -1,183 +1,75 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; UI
-(add-to-list 'custom-theme-load-path "~/.cache/emacs/themes")
-(setq doom-theme 'dankcolors)
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+(setq doom-font (font-spec :family "Maple Mono NF" :size 15 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "Maple Mono NF" :size 15))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
-(setq doom-font (font-spec :family "Maple Mono NF" :size 14.0)
-      doom-variable-pitch-font (font-spec :family "Maple Mono NF" :size 14.0))
-(add-to-list 'default-frame-alist '(font . "Maple Mono NF-15:cv01=1:cv61=1:calt=0:zero=0:cv02=0"))
 
-;; Editing
-(setq-default visual-fill-column-width 90
-              word-wrap t)
-(add-hook 'text-mode-hook #'visual-line-mode)
-(add-hook 'prog-mode-hook #'visual-line-mode)
-(add-hook 'prog-mode-hook #'rainbow-mode)
-(setq scroll-conservatively 101
-      scroll-margin 1)
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
 
-;; Meow
-(defun meow-setup ()
-  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
-  (meow-motion-overwrite-define-key
-   '("j" . meow-next)
-   '("k" . meow-prev)
-   '("<escape>" . ignore))
-  (meow-leader-define-key
-   '("j" . "H-j")
-   '("k" . "H-k")
-   '("1" . meow-digit-argument)
-   '("2" . meow-digit-argument)
-   '("3" . meow-digit-argument)
-   '("4" . meow-digit-argument)
-   '("5" . meow-digit-argument)
-   '("6" . meow-digit-argument)
-   '("7" . meow-digit-argument)
-   '("8" . meow-digit-argument)
-   '("9" . meow-digit-argument)
-   '("0" . meow-digit-argument)
-   '("/" . meow-keypad-describe-key)
-   '("?" . meow-cheatsheet))
-  (meow-normal-define-key
-   '("0" . meow-expand-0)
-   '("1" . meow-expand-1)
-   '("2" . meow-expand-2)
-   '("3" . meow-expand-3)
-   '("4" . meow-expand-4)
-   '("5" . meow-expand-5)
-   '("6" . meow-expand-6)
-   '("7" . meow-expand-7)
-   '("8" . meow-expand-8)
-   '("9" . meow-expand-9)
-   '("-" . negative-argument)
-   '(";" . meow-reverse)
-   '("," . meow-inner-of-thing)
-   '("." . meow-bounds-of-thing)
-   '("[" . meow-beginning-of-thing)
-   '("]" . meow-end-of-thing)
-   '("a" . meow-append)
-   '("A" . meow-open-below)
-   '("b" . meow-back-word)
-   '("B" . meow-back-symbol)
-   '("c" . meow-change)
-   '("d" . meow-delete)
-   '("D" . meow-backward-delete)
-   '("e" . meow-next-word)
-   '("E" . meow-next-symbol)
-   '("f" . meow-find)
-   '("g" . meow-cancel-selection)
-   '("G" . meow-grab)
-   '("h" . meow-left)
-   '("H" . meow-left-expand)
-   '("i" . meow-insert)
-   '("I" . meow-open-above)
-   '("j" . meow-next)
-   '("J" . meow-next-expand)
-   '("k" . meow-prev)
-   '("K" . meow-prev-expand)
-   '("l" . meow-right)
-   '("L" . meow-right-expand)
-   '("m" . meow-join)
-   '("n" . meow-search)
-   '("o" . meow-block)
-   '("O" . meow-to-block)
-   '("p" . meow-yank)
-   '("q" . meow-quit)
-   '("Q" . meow-goto-line)
-   '("r" . meow-replace)
-   '("R" . meow-swap-grab)
-   '("s" . meow-kill)
-   '("t" . meow-till)
-   '("u" . meow-undo)
-   '("U" . meow-undo-in-selection)
-   '("v" . meow-visit)
-   '("w" . meow-mark-word)
-   '("W" . meow-mark-symbol)
-   '("x" . meow-line)
-   '("X" . meow-goto-line)
-   '("y" . meow-save)
-   '("Y" . meow-sync-grab)
-   '("z" . meow-pop-selection)
-   '("'" . repeat)
-   '("<escape>" . ignore)))
 
-(use-package! meow
-  :config
-  (meow-setup)
-  (meow-global-mode 1))
-
-;; Completion
-(after! company
-  (setq company-idle-delay 0.2))
-(map! :i "C-SPC" #'company-complete)
-
-;; Indent bars
-(use-package! indent-bars
-  :hook (prog-mode . indent-bars-mode)
-  :config
-  (setq indent-bars-pattern "."
-        indent-bars-width-frac 0.15
-        indent-bars-pad-frac 0.1
-        indent-bars-zigzag nil
-        indent-bars-display-on-blank-lines t))
-
-;; Tabs
-(after! centaur-tabs
-  (setq centaur-tabs-style "bar"
-        centaur-tabs-height 28
-        centaur-tabs-set-bar 'under
-        centaur-tabs-show-count nil))
-
-;; Direnv
-(after! envrc
-  (envrc-global-mode))
-
-;; LSP
-(after! eglot
-  (setq eglot-ignored-server-capabilities '())
-  (add-hook 'eglot-managed-mode-hook
-            (lambda ()
-              (eglot-inlay-hints-mode 1))))
-
-;; Modeline
-(after! doom-modeline
-  (setq doom-modeline-buffer-file-name-style 'truncate-from-project
-        doom-modeline-vcs-max-length 24
-        doom-modeline-buffer-encoding t
-        doom-modeline-check-simple-format nil))
-
-;; Flymake
-(after! flymake
-  (setq flymake-show-diagnostics-at-end-of-line 'short))
-
-;; Vertico
-(after! vertico
-  (setq completion-ignored-extensions nil))
-
-;; Projectile
-(after! projectile
-  (setq projectile-globally-ignored-directories nil))
-
-;; Keybindings
-(map! "M-j"
-      (cmd!
-       (let ((col (current-column)))
-         (forward-line 1)
-         (transpose-lines 1)
-         (forward-line -1)
-         (move-to-column col)))
-      "M-k"
-      (cmd!
-       (let ((col (current-column)))
-         (transpose-lines 1)
-         (forward-line -2)
-         (move-to-column col)))
-      "C-a"
-      (cmd!
-       (dolist (buf (buffer-list))
-         (with-current-buffer buf
-           (when (and buffer-file-name (not (buffer-modified-p)))
-             (revert-buffer t t t)))))
-      "C-i" #'eglot-inlay-hints-mode
-      "C-r" (cmd! (revert-buffer t t t)))
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `with-eval-after-load' block, otherwise Doom's defaults may override your
+;; settings. E.g.
+;;
+;;   (with-eval-after-load 'PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look them up).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
