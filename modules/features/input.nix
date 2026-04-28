ddfce9e908c6d0ecb67d21afa6be9ee151ab28ea
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-04-28 23:22:28 +0530

    updated input.nix

--- modules/features/input.nix
+++ modules/features/input.nix
@@ -18,7 +18,11 @@
 
       config = lib.mkMerge [
         (lib.mkIf config.input.kanata.enable {
+          # boot.kernelModules = [ "uinput" ];
           hardware.uinput.enable = true;
+          # services.udev.extraRules = ''
+          #   KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+          # '';
           users.groups.uinput = { };
           services.kanata = {
             enable = true;
