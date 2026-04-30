46b798c7d703c7535dd9a380bf663eb3367520c5
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-04-30 11:54:35 +0530

    updated doom emacs config

--- modules/features/editors/doom-emacs/config.el
+++ modules/features/editors/doom-emacs/config.el
@@ -21,9 +21,10 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
+(setq doom-font (font-spec :family "Maple Mono NF" :size 14.0)
+      doom-variable-pitch-font (font-spec :family "Maple Mono NF" :size 14.0))
+(add-to-list 'default-frame-alist '(font . "Maple Mono NF-15:cv01=1:cv61=1:calt=0:zero=0:cv02=0"));;
+
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
@@ -32,16 +33,16 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(add-to-list 'custom-theme-load-path "~/.cache/emacs/themes")
+(setq doom-theme 'dankcolors)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `with-eval-after-load' block, otherwise Doom's defaults may override your
